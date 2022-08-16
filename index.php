9882ce6fb6d6b464d5c463910f06e87c14543344
stribus <paladinotoldo@gmail.com>
2022-08-15 21:24:45 -0300

    criação de um proxy

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+
+    //pega o conteudo do body
+    $body = file_get_contents('php://input');
+
+    $requestUri = $_SERVER['REQUEST_URI'];
+    $httpPos = strpos($requestUri, 'http');
+    $ishttps = strpos($requestUri, 'https')>0;
+    $url = substr($requestUri, $httpPos, strlen($requestUri)-$httpPos);
+    $url = strtr($url, array('http//'=>'http://', 'https//'=>'https://'));
+    // print_r($url);
+    // var_dump(getallheaders());
+
+    $headers = getallheaders();
+    //pega  o verbo do request
+    $verb = $_SERVER['REQUEST_METHOD'];
+
+    //carrega dados do site $url
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    
+    $output = curl_exec($ch);   
+
+    if(curl_exec($ch) === false)
+    {
+        echo 'Curl error: ' . curl_error($ch);
+    }
+
+    echo $output;
+
+    curl_close($ch);
